f820a582ed90b3e3d27a64e73e111c208e70a5a4
Syed Maaz <syedmaaz78622@gmail.com>
2025-12-17 14:16:09 +0530

    Fast API learning

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Hello There! This is Syed Maaz."}
+
+@app.get("/greet/{name}")  # Here we have created a route which takes input from the url itself and prints it in the result page
+async def greet_name(name: str) -> dict:
+    return {"message":f"Welcome {name}"}
